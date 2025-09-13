01b93f2fced88fdde6f054841b4d21a6d5815a0f
omgaXD <sladekop@gmail.com>
2025-09-13 13:47:24 +0500

    fix tailwindcss

--- web/src/ts/hello-world.ts
+++ web/src/ts/hello-world.ts
@@ -1,7 +1,13 @@
 import $ from "jquery";
+import DG from "./2gis/DG";
 
 export function helloWorld() {
-    alert("Hello, World!");
+    console.log(DG);
+    var map = DG.map("map", {
+        center: [54.98, 82.89],
+        zoom: 13,
+    });
+    DG.marker([54.98, 82.89]).addTo(map).bindPopup("Hello, 2GIS!");
 }
 
 $('button').on('click', helloWorld);
